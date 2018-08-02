4213271fb2b9cc81946b2eb365b70ac291b774cb
AKI <41236290+abcAKIabc@users.noreply.github.com>
2018-08-02 15:25:44 +0900

    Add files via upload

---
+++ Game_practice(2-1).py
@@ -0,0 +1,22 @@
+print("Hello")
+
+print(2 + 3)
+print(7 - 4)
+print(7 * 4)
+print(7 / 4)
+print(int(2 + 3) * 4)
+
+print(7 % 4)
+print(11 % 3)
+print(7 // 4)
+print(3 ** 2)
+print(3 ** 3)
+print(3 ** 4)
+
+print(divmod(11, 4))
+
+print(2 * 5)
+print(5 ** 9)
+print(int(3 + 2) * 9)
+
+print(divmod(14, 3))
