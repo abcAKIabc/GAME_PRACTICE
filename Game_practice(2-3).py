0755fc4410123db8d6e9cb38ccb9acbdadca55fb
AKI <41236290+abcAKIabc@users.noreply.github.com>
2018-08-04 02:35:24 +0900

    Add files via upload

---
+++ Game_practice(2-3).py
@@ -0,0 +1,31 @@
+a = 4
+a = a + 3
+print(a)
+
+b = 8
+b = b - 4
+print(b)
+
+a = 5
+a += 1
+print(a)
+
+b = 9
+b -= 3
+print(b)
+
+a = 4
+a *= 4
+print(a)
+
+a = 6
+a /= 2
+print(a)
+
+a = 7
+a **= 2
+print(a)
+
+a = 7
+a //= 2
+print(a)
