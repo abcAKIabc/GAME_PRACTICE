7aedf9bfe6427bff1b143bb6753d9f2b0f604362
AKI <41236290+abcAKIabc@users.noreply.github.com>
2018-08-04 03:08:42 +0900

    Add files via upload

---
+++ Game_practice(2-5).py
@@ -0,0 +1,43 @@
+print(type(6))
+print(type(9.2))
+print(type(-10))
+print(type(-9.382))
+
+a = 8
+b = 4
+c = a + b
+print(type(c))
+
+c = a / b
+print(c)
+print(type(c))
+
+a = 7.3
+b = 5
+c = a + b
+print(c)
+print(type(c))
+
+
+a = 8.983
+b = 7.8746
+c = a + b
+print(c)
+print(type(c))
+
+
+a = "GoodMorning"
+b = "GF"
+print(a)
+print(b)
+print(a + " " + b)
+
+a = 'she says "Hello!" to me'
+print(a)
+
+#a = "she says "Hello!" to me"
+
+b = "I'm angry"
+print(b)
+
+#b = 'I'm angry'
